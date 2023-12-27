14f86caaaa7d3f6a9a5d18644f6d1d9217ecfd1c
Vinom203 <122938952+Vinom203@users.noreply.github.com>
2023-12-27 13:56:36 +0300

    Update install_tts.sh

--- install_tts.sh
+++ install_tts.sh
@@ -4,10 +4,10 @@
 mkdir -p ~/bin
 
 # Download the script
-wget -O ~/bin/tts https://example.com/path/to/your/script.py
+wget -O ~/bin/tts https://github.com/Vinom203/text-to-speach-command/blob/ff3ac6736a9c3bf42e09becfc62f6da124de77d4/script.py
 
 # Make it executable
-chmod +x ~/bin/mycommand
+chmod +x ~/bin/tts
 
 # Add the directory to the PATH (assuming it's not already added)
 echo 'export PATH="$PATH:~/bin"' >> ~/.bashrc   # or ~/.bash_profile, ~/.zshrc, etc.
